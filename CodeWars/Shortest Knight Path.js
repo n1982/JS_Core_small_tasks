667e1aef94551370822c632076e9a0ba52541082
Nick Selivanov <edu.selivanov@ya.ru>
2023-09-05 09:17:11 +0300

    unresolved task

---
+++ CodeWars/Shortest Knight Path.js
@@ -0,0 +1,6 @@
+// todo решить кату
+/* 
+Given two different positions on a chess board, find the least number of moves it would take a knight to get from one to the other. 
+The positions will be passed as two arguments in algebraic notation. For example, knight("a3", "b5") should return 1.
+The knight is not allowed to move off the board. The board is 8x8.
+*/
