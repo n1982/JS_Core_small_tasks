3384236183c4ebdd82551ee6a08ead4ef977a1d2
Nick Selivanov <edu.selivanov@yandex.ru>
2024-04-20 08:14:43 +0300

    findAllIdx to ts

---
+++ 1 JS Core/1.2.10_findAllIdx.ts
@@ -0,0 +1,13 @@
+/*findAllIdx
+Напишите функцию findAllIdx(arr, value), которая возвращает массив индексов элементов, у которые значение равно value.*/
+
+
+function findAllIdx<T>(arr:T[], value:T): number[] |[] {
+  return arr.map((el, index) => {
+      if (el === value) return index;
+    }).filter(index => index !== undefined);
+}
+
+console.log(findAllIdx([1, 0, 1, 0, 0, 1], 0)); // [1,3,4]
+console.log(findAllIdx([5, 2, 3, '5', 2, 5, 4], '5')); // [3]
+
