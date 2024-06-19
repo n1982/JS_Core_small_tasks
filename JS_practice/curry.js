a6b947cec4d6ada57e309b4dbfa95dda63eec079
Nick Selivanov <edu.selivanov@yandex.ru>
2024-06-19 20:07:34 +0300

    каррирование

--- JS_practice/curry.js
+++ JS_practice/curry.js
@@ -1,15 +1,9 @@
 function curry(func) {
-    console.log(func.length);
     return function curried(...args) {
-        console.log('func in curried', func.length);
-        console.log('args in curried', args.length);
-        console.log(args.length >= func.length);
         if (args.length >= func.length) {
             return func.apply(this, args);
         } else {
             return function (...args2) {
-                console.log(args);
-                console.log(args2);
                 return curried.apply(this, args.concat(args2));
             };
         }
@@ -22,7 +16,7 @@ function sum(a, b, c) {
 
 let curriedSum = curry(sum);
 
-//console.log(curriedSum(1, 2, 3)); // 6, всё ещё можно вызывать нормально
+console.log(curriedSum(1, 2, 3)); // 6, всё ещё можно вызывать нормально
 
-//console.log(curriedSum(1)(2, 3)); // 6, каррирование первого аргумента
-console.log(curriedSum(1)(2)(3)); // 6, каррирование всех аргументов
+console.log(curriedSum(1)(2, 3)); // 6, каррирование первого аргумента
+console.log(curriedSum(1)(4)(5)); // 6, каррирование всех аргументов
